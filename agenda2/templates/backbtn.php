3938cd118e8aaa918cefb84d12b3cf8652e20b42
Frederico Antonio <fredericoantonio450@gmail.com>
2026-01-05 11:34:08 -0300

    Delete agenda2 directory

--- agenda2/templates/backbtn.php
+++
@@ -1,9 +0,0 @@
-<?php /// Definindo uma URL para o Botão de Voltar
-$destino_url = "index.php"; // Adicione aspas simples para uma melhor prática
-
-?>
-
-<!--Botão-->
-<a href="<?= $destino_url; ?>"  class="btn btn-outline-primary" role="button">Voltar</a>
-
-
